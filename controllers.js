311f52c268b914b5e108b0a6018d34b51e539f84
S1rAquiles <reddragonlord72@gmail.com>
2024-01-13 13:20:42 -0400

    otro cambio

--- controllers.js
+++ controllers.js
@@ -3,7 +3,7 @@ const path = require('path');
 require('dotenv').config();
 var handlebars = require('express-handlebars')
 const Recaptcha = require('express-recaptcha').RecaptchaV2;
-const recaptcha = new Recaptcha('6LcP7EkpAAAAADhHkaAuLH8jC4G3Mp04NgwBgacw', '6LcP7EkpAAAAAGBhwpe7GvkNtCQN9q3Wc5Ma7Nvx');
+const recaptcha = new Recaptcha('6LcP7EkpAAAAADhHkaAuLH8jC4G3Mp04NgwBgacw', '6LcP7EkpAAAAAGBhwpe7GvkNtCQN9q3Wc5Ma7Nvx', '6Ldawk0pAAAAAJfXwEfSKEJVfoREZPmqeaRhIaQk');
 const multer = require('multer');
 const http = require('http');
 const express = require('express');
